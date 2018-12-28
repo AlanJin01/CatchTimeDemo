3a83ce9e44ddd88c30aee908a33ef9f69d3f8669
Kimsswift <45279017+Kimsswift@users.noreply.github.com>
2018-12-29 00:57:05 +0800

    Initial Commit

---
+++ CatchTimeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchTimeDemo
+//
+//  Created by J K on 2018/12/29.
+//  Copyright © 2018 Kims. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
